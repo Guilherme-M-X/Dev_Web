0bb353bbbfee11c0af3dff660d9caf595b1c8441
David Batista <davidpatrick43@gmail.com>
2026-04-14 22:11:36 -0300

    Implementado caixa de login e JS

---
+++ src/script.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById("modalLogin");
+var btn = document.getElementById("botaoLogin");
+var span = document.getElementsByClassName("fechar")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
